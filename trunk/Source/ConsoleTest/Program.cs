6b543e43b6c7480038c7da3097d8daa25daa0774
ahu.marktrue@gmail.com <ahu.marktrue@gmail.com@43a69c93-c4d1-35ae-89d1-ce67a73271e9>
2013-08-01 10:53:16 +0000

    add mod

---
+++ trunk/Source/ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebCrawler.Core;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchCrawlerWorker oSCWorker = new SearchCrawlerWorker("http://www.ahedu.gov.cn");
+        }
+    }
+}
